ca83fc4578ea0fe44fa973923f2418f27a059d0a
KyleCow <9131@kingdown.wilts.sch.uk>
2018-03-12 21:34:15 +0000

    Update script.js

--- maths/script.js
+++ maths/script.js
@@ -1,3 +1,18 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
 var solution =  0
 var f, m, a, sd1, sd2, hyp, x1, y1, x2, y2, midx, midy, sides, r;
 function noInput(variable) {
