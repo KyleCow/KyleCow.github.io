4202a11354bf4ca4b893f0a2d0b5d373169fb81e
KyleCow <9131@kingdown.wilts.sch.uk>
2019-01-17 20:49:47 +0000

    Create particle.js

---
+++ gravity2/particle.js
@@ -0,0 +1,21 @@
+function Particle(){
+    stroke(255);
+    fill(255);
+	this.x = width/2;
+	this.y = 0;
+	this.diameter = 10;
+	this.g = 1;
+	this.v = 0;
+	this.display = function() {
+    	ellipse(this.x, this.y, this.diameter, this.diameter);
+  	};
+	this.fall = function(){
+		if(this.y + this.diameter <= 600){
+			this.v += this.g;
+			this.y += this.v;
+		} else {
+			this.g = 0;
+            this.v = 0;
+		}
+	}
+}
