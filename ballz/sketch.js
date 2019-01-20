3a0891de37a0890fbcbb2d125a8195f775ec10a5
KyleCow <9131@kingdown.wilts.sch.uk>
2019-01-20 20:33:13 +0000

    Update sketch.js

--- ballz/sketch.js
+++ ballz/sketch.js
@@ -11,24 +11,26 @@ var linec3 = random(0,255);
 var linew = random(2, 13);
 
 function setup() {
-createCanvas(1920, 1080)
-background(0);
+	createCanvas(displayWidth, displayHeight)
+	background(0);
 }
 
 function draw() {
-	x = random(width);
-	y = random(height);
-	w = random(2,42)
-	h = random(2, 42)
-	c1 = random(0,255);
-	c2 = random(0,255);
-	c3 = random(0,255);
-	linec1 = random(0,255);
-	linec2 = random(0,255);
-	linec3 = random(0,255);
-	linew = random(2, 13);
-	stroke(linec1, linec2, linec3);
-	strokeWeight(linew)
-	fill(c1, c2, c3);
-	ellipse(x, y ,w ,h);
+	for(var i = 0; i < 100; i++){
+		x = random(width);
+		y = random(height);
+		w = random(2,42)
+		h = random(2, 42)
+		c1 = random(0,255);
+		c2 = random(0,255);
+		c3 = random(0,255);
+		linec1 = random(0,255);
+		linec2 = random(0,255);
+		linec3 = random(0,255);
+		linew = random(2, 13);
+		stroke(linec1, linec2, linec3);
+		strokeWeight(linew)
+		fill(c1, c2, c3);
+		ellipse(x, y ,w ,h);
+	}
 }
