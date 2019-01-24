2ac523f26d7c18c970cf933a424ea5a8ee63631f
KyleCow <9131@kingdown.wilts.sch.uk>
2019-01-24 18:54:52 +0000

    Update sketch.js

--- cool thing/sketch.js
+++ cool thing/sketch.js
@@ -5,7 +5,7 @@ let amt = 500;
 function setup() {
 	createCanvas(600, 600);
 	background(0);
-    for(let i = 0; i < amt; i++){	
+   	for(let i = 0; i < amt; i++){	
 		ball[i] = new Particle();
 	}
 }
