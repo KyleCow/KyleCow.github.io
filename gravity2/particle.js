5b006c9fbd6f418ba0ff03ddf36523dc49071389
KyleCow <9131@kingdown.wilts.sch.uk>
2019-01-24 18:42:55 +0000

    Update particle.js

--- gravity2/particle.js
+++ gravity2/particle.js
@@ -1,23 +1,40 @@
-function Particle(){
-    stroke(255);
-    fill(255);
-	this.x = width/2;
-	this.y = 0;
+function Particle() {
+	stroke(255);
+	fill(255);
+	this.x = random(width);
+	this.y = random(height);
 	this.diameter = 10;
 	this.g = 1;
 	this.v = 0;
+	this.vx = random(1,3);
 	this.display = function() {
-    	ellipse(this.x, this.y, this.diameter, this.diameter);
-  	};
-	this.fall = function(){
-		if(this.y + this.diameter < 600){
+		ellipse(this.x, this.y, this.diameter, this.diameter);
+	};
+	this.fall = function() {
+		if (this.y + this.diameter < 600 && this.y - this.diameter > 0) {
 			this.v += this.g;
 			this.y += this.v;
 		} else if (this.y + this.diameter > 600) {
-            this.y = 600 - this.diameter;
-            this.g = 0;
-            this.v = 0;
+			if(abs(this.v) > 4.5){
+				this.v = -this.v * 0.8;
+				this.vx =this.vx * 0.8;
+				this.y = 600 - this.diameter-4;
+			} else {
+				this.v = 0;
+			}
+			//this.g = 0;
+		} else if (this.y - this.diameter < 0) {
+			this.v = 0;
+			this.y = this.diameter + 3;
+		}
+	}
+	this.move = function(){
+		if(this.x > 600 + this.diameter){
+			this.x = 0 - this.diameter;
+		}
+		
+		if (this.y + this.diameter < 600) {
+			this.x += this.vx;
 		}
 	}
 }
-
