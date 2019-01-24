24f4263d0e584de151266442d6a1145942d2fd9a
KyleCow <9131@kingdown.wilts.sch.uk>
2019-01-24 18:53:32 +0000

    Create Particle.js

---
+++ cool thing/Particle.js
@@ -0,0 +1,17 @@
+function Particle() {
+	stroke(255);
+	fill(255);
+	this.x = random(width);
+	this.y = random(height);
+	this.v = random(1,3);
+	this.diameter = 5 * this.v;
+	this.display = function() {
+		ellipse(this.x, this.y, this.diameter, this.diameter);
+	};
+	this.move = function(){
+		if(this.x > 600 + this.diameter){
+			this.x = 0 - this.diameter;
+		}
+		this.x += this.v;
+	}
+}
