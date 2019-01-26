ef1c2b5a2fb8be5fc8a3b8d6b3fc748c94719413
KyleCow <9131@kingdown.wilts.sch.uk>
2019-01-26 21:05:54 +0000

    Update sketch.js

--- gravity2/sketch.js
+++ gravity2/sketch.js
@@ -4,7 +4,7 @@ let amt = 1000;
 
 function setup() {
 	slider = createSlider(0,3,2,0.1);
-	slider.position(0, 30);
+	slider.position(0, 20);
 	createCanvas(600, 600);
 	background(0);
     for(let i = 0; i < amt; i++){	
@@ -25,9 +25,9 @@ function draw() {
 
 function mousePressed(){
     for(let i = 0; i < amt; i++){	
-		ball[i].v = -random(5,30);
-    	ball[i].y -= 5;
-    	ball[i].g = 1;
+		ball[i].v = ball[i].jump;
+    		ball[i].y -= 5;
+    		ball[i].g = 1;
 		ball[i].vx = random(ball[i].vx,5);
 	}
 }
