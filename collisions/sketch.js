308b4863d45b3ff53a9d2b6c19fe044662750f14
KyleCow <9131@kingdown.wilts.sch.uk>
2019-06-18 16:15:50 +0100

    Update sketch.js

--- collisions/sketch.js
+++ collisions/sketch.js
@@ -30,38 +30,43 @@ function setup(){
     // slider.position(1150, 10);
     // slider.style('width', '140px');
 
-    mass1 = createSlider(0, 8, 1, 0.1);
-    mass1.position(1150, 580);
-    mass1.style('width', '140px');
+    mass1 = Number(window.prompt('mass 1 ='))
+    mass2 = Number(window.prompt('mass 2 ='))
+  
+//     mass1 = createSlider(0, 8, 1, 0.1);
+//     mass1.position(1150, 580);
+//     mass1.style('width', '140px');
 
-    mass2 = createSlider(0, 2, 1, 0.1);
-    mass2.position(1000, 580);
-    mass2.style('width', '140px');
+//     mass2 = createSlider(0, 2, 1, 0.1);
+//     mass2.position(1000, 580);
+//     mass2.style('width', '140px');
 
 }
 
 
 function draw(){
-    m1 = Math.floor(10**mass1.value());
-    m2 = Math.floor(10**mass2.value());
+    // m1 = Math.floor(10**mass1.value());
+    // m2 = Math.floor(10**mass2.value());
+    m1 = mass2;
+    m2 = mass1;
     //n = slider.value();
-    y1 = height/2 - 50*Math.log10(m1+1)
-    y2 = height/2 - 50*Math.log10(m2+1)
+    y1 = height/2 - 5*Math.pow(m1, (1/3))
+    y2 = height/2 - 5*Math.pow(m2, (1/3))
 
     clear();
 
     stroke(255);
     background(51);
-    text('Slomo Slider' ,1210, 35);
-    text('Mass1 Slider' ,1190, 565);
-    text('Mass2 Slider' ,1040, 565);
+    // text('Slomo Slider' ,1210, 35);
+    // text('Mass1 Slider' ,1190, 565);
+    // text('Mass2 Slider' ,1040, 565);
 
     text(String(hit), 30, 30)
     strokeWeight(1);
     noFill();
-    rect(x1,y1,100*Math.log10(m1+1),100*Math.log10(m1+1));
+    rect(x1,y1,10*Math.pow(m1, (1/3)),10*Math.pow(m1, (1/3)));
     text('Mass = '+m1, x1, y1-10);
-    rect(x2,y2,100*Math.log10(m2+1),100*Math.log10(m2+1));
+    rect(x2,y2,10*Math.pow(m2, (1/3)),10*Math.pow(m2, (1/3)));
     text('Mass = '+m2, x2, y2-10);
     line(10, 0, 10, 1200);
 	for(let i = 0; i < time; i++){
