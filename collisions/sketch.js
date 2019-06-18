bdebdc105ccb8be5735e83842f3e5c0a59146729
KyleCow <9131@kingdown.wilts.sch.uk>
2019-06-18 16:00:31 +0100

    Update sketch.js

--- collisions/sketch.js
+++ collisions/sketch.js
@@ -14,25 +14,27 @@ let v2o;
 
 let hit = 0;
 
-let n;
+let n = 0.2;
 
-let slider;
+//let slider;
 let mass1;
 let mass2;
 
+let time = 1000000;
+
 function setup(){
 
-    createCanvas(1300,600);
+    createCanvas(1300,1000);
 	frameRate(6000);
-    slider = createSlider(0.5, 100, 1);
-    slider.position(1150, 10);
-    slider.style('width', '140px');
+    // slider = createSlider(0.2, 100, 1);
+    // slider.position(1150, 10);
+    // slider.style('width', '140px');
 
-    mass1 = createSlider(0, 4, 1, 0.1);
+    mass1 = createSlider(0, 8, 1, 0.1);
     mass1.position(1150, 580);
     mass1.style('width', '140px');
 
-    mass2 = createSlider(0, 4, 1, 0.1);
+    mass2 = createSlider(0, 2, 1, 0.1);
     mass2.position(1000, 580);
     mass2.style('width', '140px');
 
@@ -42,9 +44,9 @@ function setup(){
 function draw(){
     m1 = Math.floor(10**mass1.value());
     m2 = Math.floor(10**mass2.value());
-    n = slider.value();
-    y1 = 600/2 - 50*Math.log10(m1+1)
-    y2 = 600/2 - 50*Math.log10(m2+1)
+    //n = slider.value();
+    y1 = height/2 - 50*Math.log10(m1+1)
+    y2 = height/2 - 50*Math.log10(m2+1)
 
     clear();
 
@@ -62,30 +64,32 @@ function draw(){
     rect(x2,y2,100*Math.log10(m2+1),100*Math.log10(m2+1));
     text('Mass = '+m2, x2, y2-10);
     line(10, 0, 10, 1200);
+	for(let i = 0; i < time; i++){
+		if (abs(x1-x2)-(100*Math.log10(m2+1))<= 0){
+			v2o = (2*m1*v1/(m1+m2))-(v2*(m1-m2)/(m1+m2));
+			v1o = ((m1-m2)*v1/(m1+m2))+(2*m2*v2/(m1+m2));
+			hit += 1;
+		} else {
+			v1o = v1;
+			v2o = v2;
+		}
 
-    if (abs(x1-x2)-(100*Math.log10(m2+1))<= 0){
-        v2o = (2*m1*v1/(m1+m2))-(v2*(m1-m2)/(m1+m2));
-        v1o = ((m1-m2)*v1/(m1+m2))+(2*m2*v2/(m1+m2));
-        hit += 1;
-    } else {
-        v1o = v1;
-        v2o = v2;
-    }
+		v1 = v1o;
+		v2 = v2o;
 
-    v1 = v1o;
-    v2 = v2o;
+		if (x1 <= 10){
+			v1 = 0 - v1;
+			x1 = 10;
+			hit += 1;
+		}
 
-    if (x1 <= 10){
-        v1 = 0 - v1;
-        hit += 1;
-    }
+		if (x2 <= 10){
+			v2 = 0 - v2;
+			x2= 10;
+			hit += 1;
+		}
 
-    if (x2 <= 10){
-        v2 = 0 - v2;
-        hit += 1;
-    }
-
-    x1 -= v1/n;
-    x2 -= v2/n;
-
+		x1 -= (v1/n)/time;
+		x2 -= (v2/n)/time;
+	}
 }
